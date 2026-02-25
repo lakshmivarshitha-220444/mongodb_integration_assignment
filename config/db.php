67f3e067856db6938045f599ff1e26afc96ef2ad
lakshmivarshitha-220444 <n220444@rguktn.ac.in@college.edu>
2026-02-25 12:36:51 +0530

    mongodb integration

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+require '../vendor/autoload.php';
+
+$mongoUri = "mongodb://localhost:27017";
+
+try {
+    $client = new MongoDB\Client($mongoUri);
+    $db = $client->userDB;
+} catch (Exception $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
